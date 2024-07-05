fdd795a95d0c0a9dc66525f73f7645a0db2e2ca2
Aaron Dabney <aarongdabney@gmail.com>
2024-07-04 20:06:09 -0700

    v2

--- eloquentjs/15_1_balloon/index.js
+++ eloquentjs/15_1_balloon/index.js
@@ -3,29 +3,20 @@ let balloon = document.getElementById("balloon");
 let balloonSize = 1;
 let explosionThreshold = 18;
 
-let timeout;
-
 let balloonHandler = event => {
     if (event.key === 'ArrowUp') {
-        clearTimeout(timeout);
-
-        timeout = setTimeout(() =>{
-            if (balloonSize > explosionThreshold) {
-                explosion();
-                return;
-            }
-            balloonSize *= 1.1;
-            balloon.style.fontSize = `${balloonSize}em`;
-        }, 50);
-
+        if (balloonSize > explosionThreshold) {
+            explosion();
+            return;
+        }
+        
+        balloonSize *= 1.1;
+        balloon.style.fontSize = `${balloonSize}em`;
+        
         event.preventDefault();
     } else if (event.key === 'ArrowDown') {
-        clearTimeout(timeout);
-
-        timeout = setTimeout(() =>{
-            balloonSize *= 0.9;
-            balloon.style.fontSize = `${balloonSize}em`;
-        }, 50);
+        balloonSize *= 0.9;
+        balloon.style.fontSize = `${balloonSize}em`;
 
         event.preventDefault();
     }
