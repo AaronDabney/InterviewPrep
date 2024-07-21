e899cab4a124cca565a718f7608d84a05713da0c
Aaron Dabney <aarongdabney@gmail.com>
2024-07-21 09:38:20 -0700

    wip

--- eloquentjs/17_2_pie_chart/main.js
+++ eloquentjs/17_2_pie_chart/main.js
@@ -1,4 +1,4 @@
-import { Polygon } from "./src/polygon.js";
+import { Polygon, drawPoly, isPolygonInPolygon, isPointInPolygon} from "./src/polygon.js";
 import { Vector2 } from "./src/vector2.js";
 
 const surveyResults = [
@@ -8,6 +8,7 @@ const surveyResults = [
     {name: "No comment", count: 175, color: "silver"}
 ];
 
+drawPieChart(surveyResults, "pieChartCanvas");
 
 function drawPieChart(results, canvasID) {
     let canvas = document.getElementById(canvasID);
@@ -25,8 +26,6 @@ function drawPieChart(results, canvasID) {
 
     let radius = Math.min(xCenter / 1.1, yCenter / 1.1);
     
-
-
     for (let result of results) {
         let sliceAngle = (result.count / total) * 2 * Math.PI;
         ctx.beginPath();
@@ -51,10 +50,7 @@ function drawPieChart(results, canvasID) {
 
         currentAngle += sliceAngle;
         let textStartingRadius = radius * 0.66;
-        let fontSize = 10;
-
-        let centroidRadius = (2 * radius * Math.sin(sliceAngle * 2)) / 6 * sliceAngle;
-
+        
         let offset = sliceAngle / 2;
 
         let textRadius = textStartingRadius;
@@ -68,8 +64,9 @@ function drawPieChart(results, canvasID) {
 
         let sliceBoundary = new Polygon([a, b, c, d], center);
 
-        drawPoly(sliceBoundary, canvasID)
+        drawPoly(sliceBoundary, canvasID);
 
+        let fontSize = 10;
         ctx.font = `${fontSize }px serif`;
 
         let metrics = ctx.measureText(label);
@@ -83,10 +80,6 @@ function drawPieChart(results, canvasID) {
         let textBottomLeft = new Vector2(-width / 2,  metrics.fontBoundingBoxDescent);
 
         let textBoundary = new Polygon([textTopLeft, textTopRight, textBottomRight, textBottomLeft], textPosition);
-
- 
-  
-
 
 
         while (isPolygonInPolygon(textBoundary, sliceBoundary)) {
@@ -108,136 +101,11 @@ function drawPieChart(results, canvasID) {
             console.log('grow');
         }
 
+        drawPoly(textBoundary, canvasID);
 
-        drawPoly(textBoundary, canvasID)
-
-
-
-        ctx.fillText(label, textPosition.x , textPosition.y)
-
-
+        ctx.fillText(label, textPosition.x , textPosition.y);
     }
+
     ctx.stroke()
-    function calculateFontTransform(text) {
-
-    }
 
 }
-
-
-drawPieChart(surveyResults, "pieChartCanvas")
-
-function drawPoly(poly, canvasID) {
-    let vertices = poly.worldVertices;
-
-    let canvas = document.getElementById(canvasID);
-    let ctx = canvas.getContext("2d");  
-
-    ctx.strokeStyle = 'red'
-
-    ctx.moveTo(vertices[0].x, vertices[0].y);
-
-    for (let i = 1; i < vertices.length + 1; i++) {
-        ctx.lineTo(vertices[i % vertices.length].x, vertices[i % vertices.length].y);
-    }
-}
-
-
-function isPolygonInPolygon(insidePolygon, outsidePolygon) {
-
-    let polyVertices = insidePolygon.worldVertices;
-    //console.log(polyVertices)
-    
-    for (let i = 0; i < polyVertices.length; i++) {
-        if (!isPointInPolygon(outsidePolygon, polyVertices[i])) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-// function isPolygonInCircle(insidePolygon, circle) {
-
-//     let polyVertices = insidePolygon.worldVertices;
-    
-//     for (let i = 0; i < polyVertices.length; i++) {
-//         if (!isPointInCircle(circle, polyVertices[i])) {
-//             return false;
-//         }
-//     }
-
-//     return true;
-// }
-
-// function isPointInCircle() {
-//     if (point.sub(cirle.position) < circle.radius)
-// }
-
-function isPointInPolygon(poly, point) {
-    let vertices = poly.worldVertices;
-    //console.log(vertices)
-
-    if (vertices.length < 3) {
-        throw "This aint no polygon";
-    } 
-    
-    let polyEvals = [];
-    for (let i = 0; i < vertices.length; i++) {
-       // console.log(vertices[i])
-       // console.log(vertices[i])
-        polyEvals.push(evaluateEdge(vertices[i], vertices[(i + 1 ) % vertices.length]));
-    }
-
-   // console.log(polyEvals)
-
-    return Math.max(...polyEvals) < 0;
-
-    function evaluateEdge(a, b) {
-        let result = a.sub(b).perp().norm().dot(point.sub(b).norm());
-        //console.log(result)
-        return result;
-    }
-}
-
-
-// function drawTriangle(triangle, canvasID) {
-
-//     let canvas = document.getElementById(canvasID);
-//     let ctx = canvas.getContext("2d");  
-//     //ctx.fillRect(0, 0, canvas.width, canvas.height)
-
-//     // let xCenter = canvas.width / 2;
-//     // let yCenter = canvas.height / 2;
-//     // let center = new Vector2(xCenter, yCenter);
-//     // let scale = 10;
-
-//     // let a = new Vector2(1, 3).mult(scale).add(center);
-//     // let b = new Vector2(2, -3).mult(scale).add(center);
-//     // let c = new Vector2(-5, -3).mult(scale).add(center);
-
-//     ctx.strokeStyle = 'red'
-
-//    // let point = new Vector2(event.x, event.y)
-
-//    let [a, b, c] = triangle;
-
-//    // console.log(isPointInTriangle(point, [a, b, c]))
-
-//     ctx.moveTo(a.x, a.y);
-//     ctx.lineTo(b.x, b.y);
-//     ctx.lineTo(c.x, c.y);
-//     ctx.lineTo(a.x, a.y);
-//     ctx.lineWidth = 1;
-
-// }
-
-// function isPointInTriangle(point, triangle) {
-//     let [vertexA, vertexB, vertexC] = triangle;
-    
-//     let a = vertexA.sub(vertexB).perp().norm().dot(point.sub(vertexB).norm());
-//     let b = vertexB.sub(vertexC).perp().norm().dot(point.sub(vertexC).norm());
-//     let c = vertexC.sub(vertexA).perp().norm().dot(point.sub(vertexA).norm());
-
-//     return Math.max(a, b, c) < 0;
-// }
--- eloquentjs/17_2_pie_chart/src/polygon.js
+++ eloquentjs/17_2_pie_chart/src/polygon.js
@@ -17,4 +17,53 @@ class Polygon {
     }
 }
 
-export { Polygon }
+function drawPoly(poly, canvasID) {
+    let vertices = poly.worldVertices;
+
+    let canvas = document.getElementById(canvasID);
+    let ctx = canvas.getContext("2d");  
+
+    ctx.strokeStyle = 'red';
+
+    ctx.moveTo(vertices[0].x, vertices[0].y);
+
+    for (let i = 1; i < vertices.length + 1; i++) {
+        ctx.lineTo(vertices[i % vertices.length].x, vertices[i % vertices.length].y);
+    }
+}
+
+function isPolygonInPolygon(insidePolygon, outsidePolygon) {
+
+    let polyVertices = insidePolygon.worldVertices;
+    
+    for (let i = 0; i < polyVertices.length; i++) {
+        if (!isPointInPolygon(outsidePolygon, polyVertices[i])) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function isPointInPolygon(poly, point) {
+    let vertices = poly.worldVertices;
+
+    if (vertices.length < 3) {
+        throw "This aint no polygon";
+    } 
+    
+    let polyEvals = [];
+    for (let i = 0; i < vertices.length; i++) {
+        polyEvals.push(evaluateEdge(vertices[i], vertices[(i + 1 ) % vertices.length]));
+    }
+
+
+    return Math.max(...polyEvals) < 0;
+
+    function evaluateEdge(a, b) {
+        return a.sub(b).perp().norm().dot(point.sub(b).norm());;
+    }
+}
+
+
+export { Polygon, drawPoly, isPolygonInPolygon, isPointInPolygon }
