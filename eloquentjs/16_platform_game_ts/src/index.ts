4a6eae5912fda775450ef58ce7e2e5612b234e31
Aaron Dabney <aarongdabney@gmail.com>
2024-09-06 16:35:25 -0700

    first pass

--- eloquentjs/16_platform_game_ts/src/index.ts
+++ eloquentjs/16_platform_game_ts/src/index.ts
@@ -1,14 +1,1 @@
-
-import { CommandData } from "./interfaces";
-import { controlMapping } from "./controlMapping"
-import { runGame } from "./runTime";
-import { GAME_LEVELS } from "./levels/gameLevels"
-
-
-const commands : CommandData = controlMapping({
-    left: "ArrowLeft",
-    right: "ArrowRight",
-    jump: "ArrowUp"
-});
-
-runGame(GAME_LEVELS, commands);
+console.log("Hello world.")
