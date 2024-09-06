f91c11870bddca9b55b652fc8928697bfe4e7bf6
Aaron Dabney <aarongdabney@gmail.com>
2024-09-06 09:49:44 -0700

    draft a

--- eloquentjs/17_3_bouncing_ball/main.js
+++ eloquentjs/17_3_bouncing_ball/main.js
@@ -1,20 +1,50 @@
-import { Vector2 } from "./src/vector2";
-
-
-function animate(frameFunc) {
+function animator(animationFrame) {
     let lastTime = null;
 
     function loop(time) {
         if (lastTime !== null) {
-           let deltaTime = time - lastTime; //time in milliseconds
-           frameFunc(deltaTime)
+           let deltaTime = time - lastTime;
+           animationFrame(deltaTime)
         }
 
         lastTime = time;
-        requestAnimationFrame(loop)
+        requestAnimationFrame(loop);
     }
 
-   requestAnimationFrame(loop)
+   requestAnimationFrame(loop);
 }
 
-// animate((x) => console.log(x))
+function bouncingBall(ctx) {
+    const canvasWidth = ctx.canvas.clientWidth;
+    const canvasHeight = ctx.canvas.clientHeight;
+
+    // Range: [-1 , 1]
+    const triangleWave = (x, period) => 2 * Math.abs( 2 * (x / period - Math.floor(x / period + 0.5))) - 1; 
+    
+    const verticalPhaseMod = Math.PI / 2;
+    const ballRadius = 50;
+    
+    const xCenter = canvasWidth / 2;
+    const yCenter = canvasHeight / 2;
+
+    let period = 5;
+    let timeInSeconds = Date.now() / 1000;
+
+    let xPosition = xCenter + triangleWave(timeInSeconds, period) * xCenter * (1 - ballRadius / xCenter);
+    let yPosition = yCenter + triangleWave(timeInSeconds + verticalPhaseMod, period) * yCenter * (1 - ballRadius / yCenter);
+
+    // Clear canvas
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+    // Draw ball
+    ctx.fillStyle = 'blue';
+    ctx.beginPath();
+    ctx.ellipse(xPosition, yPosition, ballRadius, ballRadius, 0, 0, Math.PI * 2);
+    ctx.fill();
+}
+
+
+let drawingContext =  document.getElementById("box").getContext("2d");
+
+animator(() => bouncingBall(drawingContext));
