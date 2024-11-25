4b4dcf4b8e757537cc40c57580516926169cab85
Aaron Dabney <aarongdabney@gmail.com>
2024-11-24 20:04:16 -0800

    retool

--- eloquentjs/21_skill_sharing_website/router/methods/delete.mjs
+++
@@ -1,9 +0,0 @@
-export async function DELETE(server, title) {
-
-    if (Object.hasOwn(server.talks, title)) {
-      delete server.talks[title];
-      server.updated();
-    }
-
-    return { status: 204 };
-}
--- eloquentjs/21_skill_sharing_website/router/methods/get.mjs
+++
@@ -1,8 +0,0 @@
-export async function GET(server, title) {
-    if (Object.hasOwn(server.talks, title)) {
-        return {body: JSON.stringify(server.talks[title]),
-                headers: {"Content-Type": "application/json"}};
-      } else {
-        return {status: 404, body: `No talk '${title}' found`};
-      }
-}
