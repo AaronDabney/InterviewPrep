c65dc77c298df7d391121a0515f07db73e7b8055
Aaron Dabney <aarongdabney@gmail.com>
2024-11-24 20:07:18 -0800

    first commit

---
+++ eloquentjs/21_skill_sharing_website/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const isProduction = process.env.NODE_ENV == 'production';
+const stylesHandler = 'style-loader';
+const CopyPlugin = require("copy-webpack-plugin");
+
+const config = {
+    cache: false,
+    entry: {
+        server: path.resolve(__dirname, 'src/server.ts'),
+        './public/client': './src/public/client.ts',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: "src",
+                    globOptions: {
+                        ignore: ["**/tsconfig.*", "**/*.ts", "**/*.js"],
+                    }
+                }
+            ]
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/i,
+                loader: 'ts-loader',
+                exclude: ['/node_modules/'],
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
+        fallback: {
+            'fs': false
+        }
+    },
+};
+
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+    } else {
+        config.mode = 'development';
+    }
+
+    return config;
+};
