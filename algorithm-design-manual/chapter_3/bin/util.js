878d89f258fb64a88a25d5430b73895378ef1801
Aaron Dabney <aarongdabney@gmail.com>
2025-02-05 10:01:09 -0800

    hash funcs

--- algorithm-design-manual/chapter_3/bin/util.js
+++ algorithm-design-manual/chapter_3/bin/util.js
@@ -6,8 +6,29 @@ function randomRange(min, max) {
     return Math.random() * (max - min) + min;
 }
 
+let first26Primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101];
+
+function hashWordPrime(word) {
+    word = word.toLowerCase()
+    let product = 1;
+    for (let letter of word.split('')) {
+        product *= first26Primes[letter.charCodeAt(0) - 97];
+    }
+    return product;
+}
+
+function hashWord(word) {
+    let bigNumber = 26 ** word.length;
+    let sum = 0;
+    for (let i = 0; i < word.length; i++) {
+        sum = 26 ** (word.length - i + 1) * (word[i].charCodeAt(0) - 97)
+    }
+    return bigNumber + sum
+}
 
 module.exports = {
     clamp,
-    randomRange
+    randomRange,
+    hashWord,
+    hashWordPrime
 }
